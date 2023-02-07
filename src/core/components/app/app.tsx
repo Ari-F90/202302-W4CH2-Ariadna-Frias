c820ee5d7a5d4b8040c29336c4e549c8ad08c9e3
Ariadna Frias <ariadna.812@gmail.com>
2023-02-07 20:52:57 +0100

    Update app tsx file

--- src/core/components/app/app.tsx
+++ src/core/components/app/app.tsx
@@ -1,12 +1,13 @@
 import { Header } from "../header/header";
 import { Footer } from "../footer/footer";
-import { Form } from "../form/form";
+import { Form1 } from "../../../features/form/components/form1/form1";
 
 export function App() {
   return (
     <div className="container">
+      Form
       <Header></Header>
-      <Form></Form>
+      <Form1></Form1>
       <Footer></Footer>
     </div>
   );
